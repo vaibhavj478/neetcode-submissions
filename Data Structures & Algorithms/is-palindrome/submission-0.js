2293d8540740bcd4980b82980c2f10b376485d9a
Vaibhav Joshi <84984397+vaibhavj478@users.noreply.github.com>
2026-07-29 19:54:15 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+
+const  str =  s.toLowerCase().replace(/[^a-z0-9]/g, "")
+
+let left = 0 ;
+let right =  str.length - 1;  
+
+while(left<= right){
+
+
+   if(str[left] != str[right]){
+
+    return false
+   }
+
+   left++;
+   right--;
+
+
+}
+return true 
+
+    }
+}
